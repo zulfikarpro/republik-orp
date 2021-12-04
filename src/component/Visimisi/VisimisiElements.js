3eefa8f162987b59550625231122a2cba7e9e74c
muhamad zulfikar <zulfikarproduction@gmail.com>
2021-12-04 22:09:30 +0700

    fixing 041221

--- src/component/Visimisi/VisimisiElements.js
+++ src/component/Visimisi/VisimisiElements.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
-import image1 from '../../images/test.png'
 export const VisimisiContainer = styled.div`
     /* margin-top: 100px;
     margin-bottom: 100px; */
     height: 90vh;
-    /* background-image: ${props=>props.img? 'url('+image1+'),linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0, 0.4))' : ''}; */
     /* background: blue; */
     width: 100%;
     align-items: center;
